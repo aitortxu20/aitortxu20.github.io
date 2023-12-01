d7decc250a25f689bb9b98a81e6839fc6100c76b
aitortxu20 <aitortxuboy@gmail.com>
2023-12-01 20:57:52 +0100

    a

--- pwned.js
+++ pwned.js
@@ -1,15 +1,12 @@
 alert("Alerta con img");
 
-// Ejemplo con Node.js usando Express
-const express = require('express');
-const app = express();
+fetch('https://api.ipify.org?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const userIP = data.ip;
+    alert(userIP);
+  })
+  .catch(error => {
+    alert('Error al obtener la IP:', error);
+  });
 
-app.get('/', function(req, res) {
-  const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  alert(userIP);
-  res.send('¡Hola!');
-});
-
-app.listen(3000, () => {
-  alert('Servidor corriendo en el puerto 3000');
-});
