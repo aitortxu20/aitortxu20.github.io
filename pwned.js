2ce3f332bbb99e74d5cd1a4b6958ac30017b97d3
aitortxu20 <aitortxuboy@gmail.com>
2023-12-01 21:18:03 +0100

    a

--- pwned.js
+++ pwned.js
@@ -1,12 +1,4 @@
 alert("Alerta con img");
 
-fetch('https://api.ipify.org?format=json')
-  .then(response => response.json())
-  .then(data => {
-    const userIP = data.ip;
-    alert(userIP);
-  })
-  .catch(error => {
-    alert('Error al obtener la IP:', error);
-  });
+
 
