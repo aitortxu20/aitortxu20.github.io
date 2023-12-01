95b6d5a726a429284671588d88bf62405b3bea5c
aitortxu20 <aitortxuboy@gmail.com>
2023-12-01 20:54:21 +0100

    a

--- pwned.js
+++ pwned.js
@@ -1,1 +1,15 @@
 alert("Alerta con img");
+
+// Ejemplo con Node.js usando Express
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+  const userIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  alert(userIP);
+  res.send('¡Hola!');
+});
+
+app.listen(3000, () => {
+  alert('Servidor corriendo en el puerto 3000');
+});
